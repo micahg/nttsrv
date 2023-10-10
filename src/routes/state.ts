7f8ae11c8b9709be25ac4ef9711a7b87c3c23c09
Micah Galizia <micahgalizia@gmail.com>
2023-10-09 21:35:01 -0400

    add state/tabletop

--- src/routes/state.ts
+++ src/routes/state.ts
@@ -1,16 +1,38 @@
+/**TODO THIS SHOULD BE TABLETOP */
 import { Request, Response } from "express";
-import { ASSETS_UPDATED_SIG } from "../utils/constants";
-import { getTableState, TableState } from "../utils/tablestate";
+import { ASSETS_UPDATED_SIG, OBJECT_ID_LEN } from "../utils/constants";
+import { getTableState, Rect, TableState } from "../utils/tablestate";
 import { log } from "../utils/logger";
+import { getUser, userExistsOr401 } from "../utils/user";
+import { getOrCreateTableTop, getTableTopByUser, setTableTopByScene } from "../utils/tabletop";
+import { Scene } from "../models/scene";
+import { getSceneById } from "../utils/scene";
+import { validateViewPort } from "../utils/viewport";
 
 export function updateState(req: Request, res: Response, next: any) {
+    const sceneID: string = req.body.scene;
+    if (!sceneID) throw new Error('Missing scene id', {cause: 400});//return res.sendStatus(400);
+    if (sceneID.length !== OBJECT_ID_LEN) throw new Error('Invalid scene id', {cause:400});//return res.sendStatus(400);
+
+    return getUser(req.auth)
+      .then(user => userExistsOr401(user))
+      .then(user => getOrCreateTableTop(user))  // maybe we can skip this and just update by ID
+      .then(table => setTableTopByScene(table._id.toString(), sceneID))
+      .then(table => getSceneById(sceneID, table.user.toString()))
+      .then(scene => res.app.emit(ASSETS_UPDATED_SIG, scene))
+      .then(() => res.sendStatus(200))
+      .catch(err => next(err));
   let state: TableState = getTableState();
   res.app.emit(ASSETS_UPDATED_SIG, state);
   return res.json(state);
 }
 
 export function getState(req: Request, res: Response, next: any) {
-  log.info(`${JSON.stringify(req.auth.payload.sub)}`);
-  res.json(getTableState());
-  next();
+  return getUser(req.auth)
+    .then(user => userExistsOr401(user))
+    .then(user => getTableTopByUser(user))
+    .then(table => res.json(table))
+    .catch(err => next(err));
+  // log.info(`${JSON.stringify(req.auth.payload.sub)}`);
+  // return res.json(getTableState());
 }
--- src/utils/websocket.ts
+++ src/utils/websocket.ts
@@ -8,66 +8,109 @@ import { ASSETS_UPDATED_SIG } from './constants';
 
 import { log } from "./logger";
 import { getTableState, TableState } from './tablestate';
+import { getFakeUser } from './auth';
+import { IScene } from '../models/scene';
 
 interface WSStateMessage {
   method?: string,
   state?: TableState,
 }
 
+const AUD: string = process.env.AUDIENCE_URL || 'http://localhost:3000/';
+const ISS: string = process.env.ISSUER_URL   || 'https://nttdev.us.auth0.com/';
 const AUTH_REQURIED: boolean = process.env.DISABLE_AUTH?.toLowerCase() !== "true";
+const SOCKET_SESSIONS: Map<string, WebSocket> = new Map();
+
+
+function getVerifiedToken(token: string, pem: string) {
+  if (!AUTH_REQURIED) return { sub: getFakeUser() };
+  return verify(token, pem, { audience: AUD, issuerBaseURL: ISS, tokenSigningAlg: 'RS256' });
+}
+
+
+function verifyConnection(sock: WebSocket, req: IncomingMessage, pem: string) {
+  log.info(`Websocket connection established ${req.socket.remoteAddress}`);
+  let jwt: any;
+  try {
+    const parsed = new URL(req.url, `http://${req.headers.host}`);
+    const token = parsed.searchParams.get('bearer');
+    if (!token) throw new Error('Token not present');
+    jwt = getVerifiedToken(token, pem);
+  } catch (err) {
+    if (err.hasOwnProperty('message')) {
+      log.error(`WS token fail: ${err.message} (${JSON.stringify(err)})`);
+    } else {
+      log.error(`WS token fail: ${err}`);
+    }
+    sock.close();
+    return;
+  }
+
+  // kill the old session (this means we only ever have one client)
+  const user = jwt.sub;
+  if (SOCKET_SESSIONS.has(user)) {
+    SOCKET_SESSIONS.get(user).close();
+    SOCKET_SESSIONS.delete(user);
+  }
+  SOCKET_SESSIONS.set(user, sock);
+
+  let state: TableState = getTableState();
+  // don't send a partial display without overlay by accident
+  if (state.overlay === null) {
+    state = null;
+  }
+  let msg: WSStateMessage = {
+    'method': 'connection',
+    'state': state,
+  }
+  sock.send(JSON.stringify(msg));
+  sock.on('message', (buf) => {
+    let data = buf.toString();
+    log.info(`Received "${data}"`);
+  });
+}
 
 export function startWSServer(nodeServer: Server, app: Express, pem: string) {
   log.info('starting websocket server');
   let wss = new WebSocketServer({server: nodeServer});
   let emitter = app as EventEmitter;
 
-  emitter.on(ASSETS_UPDATED_SIG, (update: TableState) => {
-    wss.clients.forEach((sock:WebSocket) => {
-      let msg: WSStateMessage = {
-        'method': ASSETS_UPDATED_SIG,
-        'state': update,
-      }
-      console.log(`Sending ${JSON.stringify(msg)}`)
-      sock.send(JSON.stringify(msg));
-    });
-  });
-
-  const aud: string = process.env.AUDIENCE_URL || 'http://localhost:3000/';
-  const iss: string = process.env.ISSUER_URL   || 'https://nttdev.us.auth0.com/';
-
-  wss.on('connection', (sock: WebSocket, req: IncomingMessage) => {
-    log.info(`Websocket connection established ${req.socket.remoteAddress}`);
-    if (AUTH_REQURIED) {
-      try {
-        const parsed = new URL(req.url, `http://${req.headers.host}`);
-        const token = parsed.searchParams.get('bearer');
-        if (!token) throw new Error('Token not present');
-        // returns decoded token BUT more importantly raises exception on failure
-        verify(token, pem, { audience: aud, issuerBaseURL: iss, tokenSigningAlg: 'RS256' })
-      } catch (err) {
-        if (err.hasOwnProperty('message')) {
-          log.error(`WS token fail: ${err.message} (${JSON.stringify(err)})`);
-        } else {
-          log.error(`WS token fail: ${err}`);
-        }
-        sock.close();
-        return;
-      }
+  emitter.on(ASSETS_UPDATED_SIG, (update: IScene) => {
+    const userID = update.user.toString();
+    if (!SOCKET_SESSIONS.has(userID)) return;
+    let tableState: TableState = {
+      overlay: update.overlayContent,
+      background: update.tableContent,
+      viewport: update.viewport,
+      backgroundSize: update.backgroundSize,
     }
-    let state: TableState = getTableState();
-    // don't send a partial display without overlay by accident
-    if (state.overlay === null) {
-      state = null;
+    const sock: WebSocket = SOCKET_SESSIONS.get(userID);
+    const msg: WSStateMessage = {
+      method: ASSETS_UPDATED_SIG,
+      state: tableState,
     }
-    let msg: WSStateMessage = {
-      'method': 'connection',
-      'state': state,
-    }
+    log.info(`Sending ${JSON.stringify(msg)}`);
     sock.send(JSON.stringify(msg));
-    sock.on('message', (buf) => {
-      let data = buf.toString();
-      log.info(`Received "${data}"`);
-    });
+    // wss.clients.forEach((sock:WebSocket) => {
+    //   let msg: WSStateMessage = {
+    //     'method': ASSETS_UPDATED_SIG,
+    //     'state': tableState,
+    //   }
+    //   console.log(`Sending ${JSON.stringify(msg)}`)
+    //   sock.send(JSON.stringify(msg));
+    // });
   });
+  // emitter.on(ASSETS_UPDATED_SIG, (update: TableState) => {
+  //   wss.clients.forEach((sock:WebSocket) => {
+  //     let msg: WSStateMessage = {
+  //       'method': ASSETS_UPDATED_SIG,
+  //       'state': update,
+  //     }
+  //     console.log(`Sending ${JSON.stringify(msg)}`)
+  //     sock.send(JSON.stringify(msg));
+  //   });
+  // });
+
+  wss.on('connection', verifyConnection);
   return wss;
 }
